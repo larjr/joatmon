dcfbfb84a69e10a788391cfebf3448f7f64d12f9
luisangelorjr <luisangelorjr@gmail.com>
2022-09-11 23:30:43 -0300

    WIP: Template with swagger and rest api ok

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # joatmon
 JoATMoN - Jack of All Trades, Master of None.
+
+
+http://localhost:8080/joatmon/api/v1/hello-world
+http://localhost:8080/joatmon/api/swagger-ui/
---
+++ src/main/java/br/com/larjr/joatmon/rest/HelloWorldRest.java
@@ -0,0 +1,20 @@
+package br.com.larjr.joatmon.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.larjr.joatmon.dto.HelloWorldDTO;
+
+@RestController
+@RequestMapping("/v1/hello-world")
+public class HelloWorldRest {
+	
+	@GetMapping("")
+	public ResponseEntity<HelloWorldDTO> hello(){
+		return new ResponseEntity<>(new HelloWorldDTO(), HttpStatus.OK);
+	}
+
+}
