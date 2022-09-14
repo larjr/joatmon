da8a0ae0cb0352ac68887a82f42b3a22e6c8694f
luisangelorjr <luisangelorjr@gmail.com>
2022-09-14 20:00:29 -0300

    ADD: PlaygroundDTO for explanations at 'mentoria'

---
+++ src/main/java/br/com/larjr/joatmon/dto/PlaygroundDTO.java
@@ -0,0 +1,14 @@
+package br.com.larjr.joatmon.dto;
+
+/**
+ * Uma simples classes para ficar utilizando de exemplo durante as explicacoes
+ * da mentoria
+ * 
+ * @author @luisangelorjr
+ */
+public class PlaygroundDTO {
+
+	private char exemploDeChar;
+	private String exemploDeString;
+
+}
