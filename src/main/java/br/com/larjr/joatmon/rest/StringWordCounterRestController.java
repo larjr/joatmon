dc142908d03326ad5fd7a67b82d0d5e3e467ac9b
Angela <commercial.araujo@gmail.com>
2022-11-14 22:13:34 -0300

    Criado o contador de ocorrência de palavras

---
+++ src/main/java/br/com/larjr/joatmon/rest/StringWordCounterRestController.java
@@ -0,0 +1,44 @@
+package br.com.larjr.joatmon.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.larjr.joatmon.dto.request.StringWordCounterRequest;
+import br.com.larjr.joatmon.dto.response.StringWordCounterResponse;
+
+@RestController
+@RequestMapping("/v1/string")
+public class StringWordCounterRestController {
+	
+	@PostMapping("/word-counter")
+	public ResponseEntity<StringWordCounterResponse> hello(@RequestBody StringWordCounterRequest request){
+		StringWordCounterResponse response = new StringWordCounterResponse();
+		String texto = request.getText();
+		String palavra = request.getWord();
+		response.setStringWordCounterRequest(request);
+		response.setWordOccurrenceCounter(contarOcorrenciaDePalavras(texto, palavra));
+		
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+	private Long contarOcorrenciaDePalavras(String texto, String palavra) {
+		
+		String[] pontos = {".", ",", "!", "?", ":"};
+		for (int p = 0; p < pontos.length; p++) {
+			
+			texto = texto.replace(pontos[p], "");
+		}
+		String[] palavras = texto.split(" ");
+		int contador = 0;
+		for(int i = 0; i < palavras.length; i++) {
+			
+			if (palavras[i].equalsIgnoreCase(palavra)) {
+				contador = contador + 1;
+			}
+		}
+		return Long.valueOf(contador);
+	}
+}
