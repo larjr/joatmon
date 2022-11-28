500f7c0c1c6430e6fbf38ed543ece54f0c4702e3
Angela <commercial.araujo@gmail.com>
2022-11-28 18:31:20 -0300

    Criado o divisor de texto

---
+++ src/main/java/br/com/larjr/joatmon/dto/request/StringSplitTextRequest.java
@@ -0,0 +1,42 @@
+package br.com.larjr.joatmon.dto.request;
+
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class StringSplitTextRequest {
+	@JsonProperty(required = true)
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(text);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StringSplitTextRequest other = (StringSplitTextRequest) obj;
+		return Objects.equals(text, other.text);
+	}
+
+	@Override
+	public String toString() {
+		return "StringSplitTextRequest [text=" + text + "]";
+	}
+	
+	
+}
